5782d167351aa778636a038b16040bbcae3e4c00
Biggie <code@bigo.lu>
2026-04-05 18:34:07 -0400

    Update

--- algo-monster/.envrc-recommended.bash
+++ algo-monster/.envrc-recommended.bash
@@ -1,5 +1,5 @@
 source_url 'https://raw.githubusercontent.com/bigolu/system-configurations/refs/heads/master/direnv/plugins/direnv-manual-reload.bash' 'sha256-cJlHaRrQkPf6BY/mcQi/rrBQmESbGRBqdurDQ5/Q20w='
 direnv_manual_reload
 source_url 'https://raw.githubusercontent.com/bigolu/system-configurations/refs/heads/master/direnv/plugins/devshell-direnv.bash' 'sha256-jdYpPcz6VTJOuuYyDDqIn1MutsLUds/WI9xsEUcCZ1U='
-DEVSHELL_DIRENV_FALLBACK=true use_devshell ".#devShells.$(uname -m)-$(uname -s | tr '[:upper:]' '[:lower:]').default"
+DEVSHELL_DIRENV_FALLBACK=true use_devshell --file flake-compat.nix default
 
--- algo-monster/flake.nix
+++ algo-monster/flake.nix
@@ -3,6 +3,7 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     blueprint = { url = "github:numtide/blueprint"; inputs.nixpkgs.follows = "nixpkgs"; };
     devshell = { url = "github:numtide/devshell"; inputs.nixpkgs.follows = "nixpkgs"; };
+    flake-compat.url = "https://git.lix.systems/lix-project/flake-compat/archive/main.tar.gz";
   };
 
   outputs = inputs: inputs.blueprint { inherit inputs; };
