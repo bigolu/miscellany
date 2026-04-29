5665911f9e1ff295edd30e7bc28ceab62bbc80c1
Biggie <code@bigo.lu>
2026-04-29 18:43:08 -0400

    Update

--- algo-monster/.envrc-recommended.bash
+++ algo-monster/.envrc-recommended.bash
@@ -1,9 +1,9 @@
 source_url \
   'https://raw.githubusercontent.com/bigolu/system-configurations/refs/heads/master/direnv/plugins/direnv-manual-reload.bash' \
-  'sha256-cJlHaRrQkPf6BY/mcQi/rrBQmESbGRBqdurDQ5/Q20w='
+  'sha256-A/bOJRYyd8fJpR5Djb0oU28ZyWF497EwyrgZzMW6qkY='
 direnv_manual_reload
 
 source_url \
   'https://raw.githubusercontent.com/bigolu/system-configurations/refs/heads/master/direnv/plugins/devshell-direnv.bash' \
-  'sha256-jdYpPcz6VTJOuuYyDDqIn1MutsLUds/WI9xsEUcCZ1U='
+  'sha256-+qXct2GDcvQ/LONqktYMOvG3v8Re/wPGpKUJGmzAivI='
 DEVSHELL_DIRENV_FALLBACK=true use_devshell --file flake-compat.nix default
